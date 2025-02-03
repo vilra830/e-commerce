556bbb1a2eab0c8ead47bda1826a5ccd8aef9946
vilra830 <villafuerte.rhea@gmail.com>
2025-02-04 00:31:48 +1100

    adds some styling

--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router";
 import { useContext } from "react";
 import { ProductsContext } from "../../context/ProductsContextProvider";
+import classes from "./ProductDetailsPage.module.scss";
 
 const ProductDetailsPage = () => {
   const { getProductById , addProductToCart } = useContext(ProductsContext);
@@ -8,14 +9,14 @@ const ProductDetailsPage = () => {
   const selectedProduct = getProductById(id);
 
   return (
-    <>
-      <h1>{selectedProduct.name}</h1>
-      <img src={selectedProduct.imageUrl} tag={selectedProduct.name} />
-      <h2>${selectedProduct.price}</h2>
-      {selectedProduct.quantity > 0 ? <button onClick={() => addProductToCart(selectedProduct)} >Add To Cart/</button> : <h3>Sold Out</h3>}
+    <div className={classes.productDetails}>
+      <h1 className={classes.productName}>{selectedProduct.name} </h1>
+      <img src={selectedProduct.imageUrl} tag={selectedProduct.name} className={classes.productImage} />
+      <h2 className={classes.productPrice}>${selectedProduct.price}</h2>
+      {selectedProduct.quantity > 0 ? <button className={classes.addToCartBtn} onClick={() => addProductToCart(selectedProduct)} >Add To Cart/</button> : <h3 className={classes.soldOut}>Sold Out</h3>}
  
 
-    </>
+    </div>
   );
 };
 
